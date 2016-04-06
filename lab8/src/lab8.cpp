2951434659c2ccb4370da1c8e9aba8249346965a
sumeshwali <sumesh.wali@gmail.com>
2016-04-06 09:07:56 +0530

    Update lab8.cpp

--- lab8/src/lab8.cpp
+++ lab8/src/lab8.cpp
@@ -14,11 +14,12 @@
 #include<unistd.h>
 #include<vector>
 using namespace std;
+#define max 50
 class mgrsrt
 {
 public:
     string fname[8];
-    string name[100];
+    string a[100];
     string names[100];
     int i;
 
@@ -32,7 +33,7 @@ public:
         while(!fp1.eof())
         {
             getline(fp1,buffer);
-            name[i]=buffer;
+            a[i]=buffer;
             i++;
             /*fp2<<buffer;
             fp2<<endl;*/
@@ -45,7 +46,7 @@ public:
 	    //int pos=fp2.tellp();
 	  // fp2.seekp(pos-2,ios::beg);
             getline(fp1,buffer);
-            name[i]=buffer;
+            a[i]=buffer;
             i++;
             /*fp2<<buffer;
             fp2<<endl;*/
@@ -58,7 +59,7 @@ public:
 		//int pos=fp2.tellp();
 	   // fp2.seekp(pos-2,ios::beg);
             getline(fp1,buffer);
-            name[i]=buffer;
+            a[i]=buffer;
             i++;
             /*fp2<<buffer;
             fp2<<endl;*/
@@ -71,7 +72,7 @@ public:
 		//int pos=fp2.tellp();
 	  // fp2.seekp(pos-2,ios::beg);
             getline(fp1,buffer);
-            name[i]=buffer;
+            a[i]=buffer;
             i++;
             /*fp2<<buffer;
             fp2<<endl;*/
@@ -84,7 +85,7 @@ public:
 		//int pos=fp2.tellp();
 	 // fp2.seekp(pos-2,ios::beg);
             getline(fp1,buffer);
-            name[i]=buffer;
+            a[i]=buffer;
             i++;
            /* fp2<<buffer;
             fp2<<endl;*/
@@ -97,7 +98,7 @@ public:
 	//	int pos=fp2.tellp();
 	  // fp2.seekp(pos-2,ios::beg);
             getline(fp1,buffer);
-            name[i]=buffer;
+            a[i]=buffer;
             i++;
            /* fp2<<buffer;
             fp2<<endl;*/
@@ -110,7 +111,7 @@ public:
 	//	int pos=fp2.tellp();
 	  //  fp2.seekp(pos-2,ios::beg);
             getline(fp1,buffer);
-            name[i]=buffer;
+            a[i]=buffer;
             i++;
            /* fp2<<buffer;
             fp2<<endl;*/
@@ -123,7 +124,7 @@ public:
 	//	int pos=fp2.tellp();
 	 // fp2.seekp(pos-2,ios::beg);
             getline(fp1,buffer);
-            name[i]=buffer;
+            a[i]=buffer;
             i++;
             /*fp2<<buffer;
             fp2<<endl;*/
@@ -133,10 +134,10 @@ public:
         fp1.close();
         cout<<"content of name :"<<endl;
         for(int j=0;j<i;j++)
-        	cout<<"name["<<j<<"] : "<<name[j]<<endl;
+        	cout<<"a["<<j<<"] : "<<a[j]<<endl;
         for(int k=0;k<i;k++)
         {
-        	fp2<<name[k];
+        	fp2<<a[k];
         	fp2<<endl;
         }
         fp2.close();
@@ -149,7 +150,6 @@ public:
 	i=j;
        cout<<"value of i : "<<i<<endl;
         string buffer;
-	
         //int rsize=0;
         fstream fp1;
         /*fp1.open("final.txt",ios::in);
@@ -166,66 +166,59 @@ public:
             cout<<"k["<<k<<"]"<<" : "<<name[k]<<endl;
         int mid=(1+i)/2;*/
         /*mgrsrt m1;
-        m1.*/mergesort(0,i-1);
+        m1.*/mergesort(a,0,i-1);
         cout<<"file sorted"<<endl;
         fp1.open("final.txt",ios::out);
         fp1.close();
-       
+       cout<<"file cleared"<<endl;
         fp1.open("final.txt",ios::out|ios::app);
         for(int k=0;k<i;k++)
         {
-            fp1<<names[k];
+            fp1<<a[k];
             fp1<<endl;
         }
         fp1.close();
     }
 
-void merge(int l,int m,int r)
+void merge(string c[max],string d[max],int l,int m,int r)
 {
-mgrsrt m1;
-    cout<<"inside merge"<<endl;
-    cout<<"content of name inside merge :"<<endl;
-	cout<<"value of l & r : "<<l<<" & "<<r<<endl;
-    for(int k=l;k<=r;k++)
-	cout<<name[k]<<endl;
-    int i=0,j=m+1,k=0,q;
-    while((i<=m)&&(j<=r))
-    {
-    	if(name[i]<=name[j])
-    	{
-    		names[k++]=name[i++];
-    	}
-    	else
-    		names[k++]=name[j++];
-    	if(i>m)
-    	{
-    		for(q=j;q<=r;q++)
-    			names[k++]=name[q];
-    	}
-    	else
-    	{
-    		for(q=i;q<=m;q++)
-    		    names[k++]=name[q];
-    	}
-    }
-	/* cout<<"content of names :"<<endl;
-                for(int j=0;j<k;j++)
-                	cout<<"names["<<j<<"] : "<<names[j]<<endl;*/
+	
+	int i,j,q,k;
+	i=l;
+	j=m+1;
+	k=l;
+	while((i<=m)&&(j<=r))
+	if(c[i]<=c[j])
+	d[k++]=c[i++];
+	else
+	d[k++]=c[j++];
+	if(i>m)			
+	for(q=j;q<=r;q++)
+	d[k++]=c[q];			
+	else			
+	for(q=i;q<=m;q++)
+	d[k++]=c[q];
+	
 }
-void mergesort(int l, int r)
+void mergesort(string a[],int l, int r)
 {
-    //cout<<"inside mergesort"<<endl;
-   /* mgrsrt m1;*/
-    if (l<r)
-    {
-	//cout<<"@"<<endl;
-        int m = (l+r)/2;
-        /*m1.*/mergesort(l, m);
-	//cout<<"@1"<<endl;
-        /*m1.*/mergesort(m+1, r);
-	//cout<<"@2"<<endl;
-        /*m1.*/merge(l,m,r);
-    }  
+	cout<<"content after mergesort is :"<<endl;
+	for(i=l;i<=r;i++)
+		cout<<"a["<<i<<"] : "<<a[i]<<endl;
+   	int m,i;
+   	string b[max];
+	if(l<r)
+	{
+		m=(l+r)/2;
+		mergesort(a,l,m);
+		mergesort(a,m+1,r);	
+		merge(a,b,l,m,r);
+		for(i=l;i<=r;i++)
+			{a[i]=b[i];}
+	}
+	/*cout<<"content after mergesort is :"<<endl;
+	for(i=l;i<=r;i++)
+		cout<<"a["<<i<<"] : "<<a[i]<<endl;*/
 }
 };
 int main()
@@ -234,5 +227,4 @@ int main()
     mgrsrt m;
     j=m.mergef();
     m.mgrsort(j);
-    cout<<"file sorted"<<endl;
 }
