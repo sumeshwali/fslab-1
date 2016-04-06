27bb01c0395ad090863c141cb629e96520affab1
sumeshwali <sumesh.wali@gmail.com>
2016-04-06 09:55:06 +0530

    Update lab4.cpp

--- lab4/src/lab4.cpp
+++ lab4/src/lab4.cpp
@@ -1,192 +1,138 @@
-//============================================================================
-// Name        : lab4.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
-
 #include<iostream>
-#include<vector>
+#include<string.h>
+#include<stdlib.h>
 #include<fstream>
 #include<sstream>
-#include<unistd.h>
 #include<sys/types.h>
-#include<sstream>
-#include<stdlib.h>
-#include<fcntl.h>
+#include<unistd.h>
 using namespace std;
-vector<int> rrn;
-int rsize=0;
-class Studentrecord{
-    string name;
-    string usn;
-    string branch;
-    int sem;
-    string buffer;
-    public:
-        void read()
-        {
-           cin.clear();
-           cin.ignore(255,'\n');
-           cout<<"enter the name :"<<endl;
-           getline(cin,name);
-           cout<<"enter the usn :"<<endl;
-           cin>>usn;
-           cin.clear();
-           cin.ignore(255,'\n');
-           cout<<"enter the branch :"<<endl;
-           getline(cin,branch);
-           cout<<"enter the semester :"<<endl;
-           cin>>sem;
-           cin.clear();
-           cin.ignore(255,'\n');
-           cout<<endl;
-        }
-        int pack()
-        {
-          string sem1;
-		string temp;
-		stringstream out;
-		out<<sem;
-		sem1=out.str();
-		temp=usn+'|'+name+'|'+branch+'|'+sem1+'$';
-		cout<<temp<<endl;
-		buffer=temp;
-               int a=buffer.size();
-               return a;
-        }
-        int write()
-	{
-		fstream fp1;
-		fp1.open("data2.txt",ios::out|ios::app);
-		fp1<<buffer;
-        int pos=fp1.tellp();
-        cout<<"pos:"<<pos<<endl;
-        fp1.close();
-        int len=buffer.size();
-        rrn.push_back(pos-len);
-        rsize++;
-        cout<<"rsize:"<<rsize<<endl;
-        cout<<"content rrn:"<<rrn[rsize-1];
-        return pos;
-	}
-        void search(int recno)
-        {
-            fstream fp1;
-            int a=rrn[recno-1];
-            cout<<"urrent record position : "<<a<<endl;
-            int b=rrn[recno];
-            cout<<"next record position : "<<b<<endl;
-            int len=b-a;
-            //cout<<"length of the record : "<<endl;
-            fp1.open("data2.txt",ios::in);
-            fp1.seekp(a,ios::beg);
-            getline(fp1,buffer,'$');
-            cout<<"Student record is :"<<buffer<<endl;
-           // unpack(len);
-            fp1.close();
-        }
-        /*void unpack(int len)
-        {
-            string s;
-            int i=0;
-            usn.erase();
-            while(buffer[i]!='|')
-            {
-            	usn+=buffer[i];
-            	i++;
-            }
-            i++;
-            name.erase();
-            while(buffer[i]!='|')
-            {
-            	name+=buffer[i];
-            	i++;
-            }
-            i++;
-            branch.erase();
-            while(buffer[i]!='|')
-            {
-		branch+=buffer[i];
-		i++;
-            }
-            i++;
-            sem=0;
-            while(i!=len)
-            {
-            	s=buffer[i];
-            	stringstream convert(s);
-		convert>>sem;
-		i++;
-            }
-            cout<<"usn is :"<<usn<<endl;
-            cout<<"name is :"<<name<<endl;
-            cout<<"branch is :"<<branch<<endl;
-            cout<<"sem is :"<<sem<<endl;
-	    }*/
+#define max 20
+class seqo
+{
+    string l1[max];
+    string l2[max];
+public:
+    int c1;
+    int c2;
+    void load(char flname[max],char flname1[max]);
+    void sort();
+    void match();
 };
+void seqo :: load(char flname[max],char flname1[max])
+{
+    int i=0;
+    c1=0;c2=0;
+    fstream fp1,fp2;
+    string buffer;
+    fp1.open(flname,ios::in);
+    fp2.open(flname1,ios::in);
+   // cout<<"opened file"<<endl;
+    while(!fp1.eof())
+    {
+        //cout<<"*";
+        getline(fp1,buffer);
+        l1[i]=buffer;
+        i++;
+        c1++;   
+    }
+    cout<<endl;
+    i=0;
+    while(!fp2.eof())
+    {
+       // cout<<"@";
+        getline(fp2,buffer);
+        l2[i]=buffer;
+        i++;
+        c2++;   
+    }
+   /*cout<<"value of c1 and c2 is:"<<c1<<endl<<c2<<endl;
+    cout<<endl;
+    cout<<"closed file"<<endl;*/
+     cout<<"content of l1:"<<endl;
+    for(i=0;i<c1;i++)
+        cout<<l1[i]<<endl;
+     cout<<"content of l2"<<endl;
+    for(i=0;i<c2;i++)
+        cout<<l2[i]<<endl;
+    fp1.close();
+    fp2.close();
+}
+void seqo :: sort()
+{
+    string temp;
+    int i=0,j=0;
+    for(i=0;i<c1;i++)
+    {
+     for(j=i+1;j<c1;j++)
+        {
+           if(l1[i]>l1[j])
+           {
+               temp=l1[i];
+               l1[i]=l1[j];
+               l1[j]=temp;
+           }
+        }    
+    }
+    for(i=0;i<c2;i++)
+    {
+     for(j=i+1;j<c2;j++)
+        {
+           if(l2[i]>l2[j])
+           {
+               temp=l2[i];
+               l2[i]=l2[j];
+               l2[j]=temp;
+           }
+        }    
+    }
+   cout<<"new content of l1:"<<endl;
+    for(i=0;i<c1;i++)
+        cout<<l1[i]<<endl;
+    cout<<"new content of l2"<<endl;
+    for(i=0;i<c2;i++)
+        cout<<l2[i]<<endl;
+}
+void seqo :: match()
+{
+     cout<<"matching records are :"<<endl;
+    int i=0,j=0,k=0;
+  while(i<c1 && j<c2)
+	{
+		if(l1[i]==l2[j])
+		{
+			cout<<l1[i]<<endl;
+			i++;
+			j++;	
+		}
+		if(l1[i]<l2[j])
+		{
+			i++;
+			continue;
+		}
+		if(l1[i]>l2[j])
+		{
+			j++;
+			continue;
+		}
+	}
+		
+}
 int main()
 {
-    fstream fp1;
-    int choice,rec;
-    Studentrecord s;
-    while(1)
-    {
-    cout<<"Enter your choice :\n1) Insert\n2) Search\n3) exit"<<endl;
-    cin>>choice;
-    switch(choice)
-    {
-        case 1:
-        {
-            string s1;
-            //fp1.open("data2.txt",ios::out);
-            //fp1.seekg(0,ios::beg);
-            //fp1<<rsize;
-            /*s1=fp1.get();
-            cout<<"s1:"<<s1<<endl;*/
-            /*stringstream convert(s1);
-            convert>>rsize;
-            cout<<"initial vlue of rsize :"<<rsize<<endl;
-            fp1.close();
-            fp1.open("data2.txt");
-            fp1.seekp(0,ios::beg);
-            fp1<<rsize;
-            fp1.close();*/
-            s.read();
-            int len=s.pack();
-            int pos=s.write();
-            cout<<"back"<<endl;
-
-            //rrn.push_back(pos-len);
-
-            /*cout<<"rsize is:"<<rsize<<endl;
-            for(int i=rsize;i>=0;i--)
-                cout<<"element"<<i<<"is : "<<rrn[i]<<endl;
-            rsize++;
-            cout<<"value of rsize is : "<<rsize<<endl;*/
-           // fp1.close();
-            /*fp1.open("data2.txt");
-            fp1.seekp(0,ios::beg);
-            fp1<<rsize;
-            fp1.close();*/
-        }
-        break;
-        case 2:
-        {
-            cout<<"enter the record number to be shown"<<endl;
-            cin>>rec;
-            s.search(rec);
-        }
-        break;
-        case 3:
-        {
-        	exit(0);
-        }
-        break;
-        default: cout<<"invalid option chosen"<<endl;
-    }
-    }
-    return 0;
+    int c1=0,c2=0;
+    char fn1[max],fn2[max];
+    cout<<"enter the first filename :";
+    cin>>fn1;
+    cout<<endl;
+    cout<<"enter the second filename :";
+    cin>>fn2;
+    cout<<endl;
+    seqo s;
+   // cout<<"before loading"<<endl;
+    s.load(fn1,fn2);
+    //cout<<"done loading"<<endl;
+    s.sort();
+    //cout<<"done sorting"<<endl;
+    s.match();
+    cout<<"program terminate"<<endl;
 }
